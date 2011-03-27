7fe2a6024dafe4f543587188b337d12025e3d95f
Jeff <jeffonia@web.de>
2011-03-27 12:41:24 +0200

    Processor Projekt hinzugefügt.

---
+++ PokerStatsProcessor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokerStatsProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PokerStats Processor started.\n");
+           
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
